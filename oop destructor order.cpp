4da608b41c9e95d28b15d60a99cffb001d27b72d
Atiba Dee 💥 <atiba.dar12345@gmail.com>
2026-01-28 01:15:30 +0500

    destructor with multiple objects

---
+++ oop destructor order.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Box {
+	public:
+	Box (){
+		cout<< "constructor gee hen ye"  << endl;
+	}
+	
+ ~Box (){
+ 	cout<< "destructor gee hen ye"  << endl;
+ }
+ 
+};
+int main (){
+	Box b1 ;
+    Box b2 ;		
+	
+}
+
